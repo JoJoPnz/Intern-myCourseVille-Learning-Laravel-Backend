1674ca1ce16d10e90c845190d95cf1ff3188ee50
Pisit Wongsripisant <act21078@gmail.com>
2022-06-09 15:52:06 +0700

    add StoreController

---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Store;
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    /**
+     * Display a listing of the resource
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return response()->json(Store::all(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $name = $request->input('name');
+        $store = new Store();
+        $store->name = $name;
+        $store->save();
+        return response()->json($store, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $store = Store::findOrFail($id);
+        return response()->json($store, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $store = Store::find($id);
+        $store->name = $request->input('name');
+        $store->save();
+        return response()->json("save successful", 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // $store = Store::find($id);
+        // $store->delete();
+        return response()->json(Store::destroy($id), 200);
+        // return response()->json("delete success", 200);
+    }
+}
