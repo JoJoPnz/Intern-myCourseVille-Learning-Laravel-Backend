9d859dc0bed81d85948e7390685b9788a13c8b6c
Pisit Wongsripisant <act21078@gmail.com>
2022-06-09 15:48:08 +0700

    add model and migrate

---
+++ app/Models/Store.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+
+    public function telephones()
+    {
+        return $this->hasMany(Telephone::class);
+    }
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
+    public function owners()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
---
+++ database/migrations/2022_06_09_084536_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->decimal("price");
+            $table->unsignedBigInteger("book_type_id");
+            $table->unsignedBigInteger("store_id");
+            $table->timestamps();
+            $table->foreign('book_type_id')->references('id')->on('book_types');
+            $table->foreign('store_id')->references('id')->on('stores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
